5265951919144eb465e8a8a28f06daa7b30bbc75
hritambhra25csu087-beep <hritambhra25csu087@ncuindia.edu>
2025-11-08 19:58:59 +0530

    Create swapping.c

---
+++ swapping.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    printf("enter the values a and b ");
+    scanf("%d%d",&a,&b);
+    {
+        a=c;
+        a=b;
+        b=c;
+        printf("enter the values a:%d and b:%d",a,b)
+    }
+    return 0;
+}
